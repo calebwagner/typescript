44ccc18b50550959b42d678929facbe3ecb2e1de
Caleb <calebwagner72@yahoo.com>
2022-08-22 15:15:56 -0500

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let sales = 12345;
+let course = 'Typescript';
+let is_published = true;
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toString);
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+let sales = 12_345;
+let course = 'Typescript';
+let is_published: boolean = true;
+
+let numbers = [1, 2, 3]
+
+numbers.forEach(n => n.toString)
+
+const enum Size {Small = 1, Medium = 2, Large = 3}
+
+let mySize: Size = Size.Medium
+console.log(mySize)
