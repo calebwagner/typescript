079aed92e89be8904bfe34584b7cf55f6eaebef4
Caleb <calebwagner72@yahoo.com>
2022-08-22 15:35:22 -0500

    function practice

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-let sales = 12_345;
-let course = 'Typescript';
+let sales: number = 12_345;
+let course: string = 'Typescript';
 let is_published: boolean = true;
 
-let numbers = [1, 2, 3]
+let numbers: number[] = [1, 2, 3];
 
 numbers.forEach(n => n.toString)
 
 const enum Size {Small = 1, Medium = 2, Large = 3}
 
 let mySize: Size = Size.Medium
-console.log(mySize)
+console.log(mySize);
+
+// properly annotating functions
+function calculateTax(income: number, taxYear = 2022) : number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000, 2022);
